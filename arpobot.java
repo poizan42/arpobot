94791c43eed6814c06178683eb78adb4365d9d63
Kasper F. Brandt <poizan@poizan.dk>
2006-09-29 21:19:18 +0000

    [Empty message]

--- arpobot.java
+++ arpobot.java
@@ -1,3 +1,5 @@
+package arpobot;
+
 import java.io.*;
 import java.net.*;
 
@@ -9,61 +11,84 @@ class irc
 	private Socket socket;
 	private BufferedWriter skriv;
 	private BufferedReader laes;
-
+	
+	public enum LogLevel {DEBUG, USERINF, CTCP, CHAN, PRIV};
 
 	public void pass (String pass) throws Exception
 	{
 		commando = "PASS " + pass + "\r\n";
-		execute(commando);
+		execute(commando, LogLevel.USERINF);
 	}
 	public void nick (String nickname) throws Exception
 	{
 		commando = "NICK " + nickname + "\r\n";
-		execute(commando);
+		execute(commando, LogLevel.USERINF);
 	}
 	public void user (String username, String realname) throws Exception
 	{
 		String mode = "8";
 		commando = "USER " + username + " " + mode + " * " + " : " + realname + "\r\n";
-		execute(commando);
+		execute(commando, LogLevel.USERINF);
 	}
 	public void pong (String pong) throws Exception
 	{
 		commando = "PONG " + pong.substring(5)+ "\r\n";
-		execute(commando);
+		execute(commando, LogLevel.DEBUG);
 	}
 	public void join (String join, String key) throws Exception
 	{
 		if (!join.startsWith("#"))
 			join = "#" + join;
 		commando = "JOIN " + join + " " + key + "\r\n";
-		execute(commando);
+		execute(commando, LogLevel.CHAN);
 	}
 	public void join (String join) throws Exception
 	{
 		if (!join.startsWith("#"))
 			join = "#" + join;
 		commando = "JOIN " + join + "\r\n";
-		execute(commando);
+		execute(commando, LogLevel.CHAN);
 	}
 	public void msg (String receiver, String text) throws Exception
 	{
-		commando = "PRIVMSG " + receiver + " :" + text + "\r\n";
-		execute(commando);
+		msg(receiver, text, LogLevel.PRIV);
 	}
+	
+	public void ctcpRequest (String receiver, String text) throws Exception
+	{
+		msg(receiver, '\u0001'+text+'\u0001', LogLevel.CTCP);
+	}
+	
+	private void msg (String receiver, String text, LogLevel logLevel) throws Exception
+	{
+		commando = "PRIVMSG " + receiver + " :" + text + "\r\n";
+		execute(commando, logLevel);
+	}
+	
 	public void notice (String receiver, String text) throws Exception
 	{
+		notice(receiver, text, LogLevel.PRIV);
+	}
+	
+	public void ctcpReply (String receiver, String text) throws Exception
+	{
+		notice(receiver, '\u0001'+text+'\u0001', LogLevel.CTCP);
+	}	
+	
+	private void notice (String receiver, String text, LogLevel logLevel) throws Exception
+	{
 		commando = "NOTICE " + receiver + " :" + text + "\r\n";
-		execute(commando);
+		execute(commando, logLevel);
 	}
 
 
 
 /******/
-	private void execute(String commando) throws Exception
+	private void execute(String commando, LogLevel logLevel) throws Exception
 	{
 		this.skriv.write(commando);
 		this.skriv.flush();
+		
 		System.out.print("<-- "+commando);
 	}
 	public void connect() throws Exception
@@ -83,13 +108,13 @@ class irc
 		this.server = server;
 		this.port = port;
 	}
-	public synchronized String getLine() throws Exception
+	public String getLine() throws Exception
 	{
 		return this.laes.readLine();
 	}
 }
 
-public class mybot
+public class arpobot
 {
 	public static void main(String[] args) throws Exception
 	{
@@ -103,7 +128,7 @@ public class mybot
 		String kanal;
 		int i;
 		String sendernick;
-		String version = "0.2 alpha";
+		String version = "arpobot svn $Revision$ $Date$";
 
 		nick = "ArvoXbot";
 		altnick = "ArvoXbotAlpha";
