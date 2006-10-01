8b379b63025f44dce33d218a0facd815ebe4f688
Kasper F. Brandt <poizan@poizan.dk>
2006-10-01 20:05:04 +0000

    [Empty message]

--- irc/command/IrcCommand.java
+++ irc/command/IrcCommand.java
@@ -1,12 +1,13 @@
 package irc.command;
 
+import java.lang.reflect.*;
 import java.util.*;
 
 public class IrcCommand
 {
 	private static HashMap<String, Class<IrcCommand>> commands = new HashMap<String, Class<IrcCommand>>();
 	
-	private String fullCommand, commandName, nick, user, hostOrNick, parameterStr;
+	private String fullCommand, commandName, nick, user, hostOrNick, paramsStr;
 
 	//Skal kaldes i en static blok i alle underklasser der tilhoerer en eller flere bestemte kommandoer
 	public static void AddCommand(Class<IrcCommand> cmd, String name)
@@ -39,9 +40,9 @@ public class IrcCommand
 		return hostOrNick;
 	}
 	
-	public String getParameterStr()
+	public String getParamsStr()
 	{
-		return parameterStr;
+		return paramsStr;
 	}
 	
 	public String toString()
@@ -49,14 +50,84 @@ public class IrcCommand
 		return fullCommand;
 	}
 	
-	protected void doParse(String params)
+	protected IrcCommand() //default konstruktoren skal ikke kunne kaldes udefra
 	{
 	}
 	
-	public static IrcCommand parse(String cmdstr)
+	//Alle underklasser skal implementere denne
+	protected IrcCommand(String pFullCommand, String command, String[] prefix, String params)
 	{
-		//TODO: Implementer :)
-		return null;
+		fullCommand = pFullCommand;
+		commandName = command;
+		if ((prefix != null) && (prefix.length == 3))
+		{
+			hostOrNick = prefix[0];
+			nick = prefix[1];
+			user = prefix[2];
+		}
+		paramsStr = params;
+	}
+	
+	public static IrcCommand parse(String cmdstr) throws 
+		NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException
+	{
+	/*
+    message    =  [ ":" prefix SPACE ] command [ params ] crlf
+    prefix     =  servername / ( nickname [ [ "!" user ] "@" host ] )
+    command    =  1*letter / 3digit
+    params     =  *14( SPACE middle ) [ SPACE ":" trailing ]
+               =/ 14( SPACE middle ) [ SPACE [ ":" ] trailing ]
+
+    nospcrlfcl =  %x01-09 / %x0B-0C / %x0E-1F / %x21-39 / %x3B-FF
+                    ; any octet except NUL, CR, LF, " " and ":"
+    middle     =  nospcrlfcl *( ":" / nospcrlfcl )
+    trailing   =  *( ":" / " " / nospcrlfcl )
+
+    SPACE      =  %x20        ; space character
+    crlf       =  %x0D %x0A   ; "carriage return" "linefeed"
+	*/
+		int i;
+		String[] prefixInf = null;
+		String fullCommand, commandName, params;
+		
+		if ((cmdstr == null) || (cmdstr.length() == 0))
+			return null;
+		
+		fullCommand = cmdstr;
+		
+		if (cmdstr.charAt(0) == ':') //der er et praefiks
+		{
+			i = cmdstr.indexOf(' ');
+			
+			if ((i == -1) || (cmdstr.length() == i+1)) //burde være falsk - kommandonavnet skal staa efter praefikset
+				return new IrcCommand(cmdstr, null, null, null);
+				
+			prefixInf = parsePrefix(cmdstr.substring(1, i));
+			
+			cmdstr = cmdstr.substring(i+1);
+		}
+		
+		//finder kommandonavnet
+		i = cmdstr.indexOf(' ');
+		if ((i == -1) || (cmdstr.length() == i+1)) //ingen parametre - ingenting der skal parses specifikt
+		{
+			commandName = cmdstr;
+			params = null;
+		}
+		else
+		{
+			commandName = cmdstr.substring(0, i);
+			params = cmdstr.substring(i+1);
+		}
+		
+		//laver en instans af den rigtige klasse
+		Class<IrcCommand> commandClass = commands.get(commandName);
+		if (commandClass == null) //der er ikke registreret nogen klasse
+			return new IrcCommand(cmdstr, commandName, prefixInf, params);
+		//finder konstruktoren
+		//String pFullCommand, String command, String[] prefix, String params
+		Constructor<IrcCommand> constructor = commandClass.getConstructor(String.class, String.class, String[].class, String.class);
+		return constructor.newInstance(fullCommand, commandName, prefixInf, params);
 	}
 
 	/*Parser prefix. Output: 
@@ -75,12 +146,12 @@ public class IrcCommand
 			return new String[]{prefix, null, null};
 		
 		host = prefix.substring(i+1);
-		nick = prefix.substring(0, i-1); //nick indeholder enten nickname eller nickname!user
+		nick = prefix.substring(0, i); //nick indeholder enten nickname eller nickname!user
 		i = nick.indexOf('!');
 		if (i == -1)
 			return new String[]{host, nick, null};
 		user = nick.substring(i+1);
-		nick = nick.substring(0, i-1);
+		nick = nick.substring(0, i);
 		
 		return new String[]{host, nick, user};
 	}
